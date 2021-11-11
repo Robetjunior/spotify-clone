de0c8e8d6bbfc9aadfea6e3f05548f9ee76d118f
Jose Roberto <jose.ferreira@77sol.com.br>
2021-11-11 01:39:40 -0300

    update Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import './Footer.css';
+import {
+  PlayCircleFilledOutlined,
+  SkipPrevious,
+  SkipNext,
+  Shuffle,
+  Repeat,
+  VolumeDown,
+  PlaylistPlay,
+} from '@material-ui/icons';
+
+import { Grid, Slider } from '@material-ui/core';
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>I am the footer</h1>
+      <div className="footer__left">
+        <div className="footer__albumLogo">
+          <img src="https://upload.wikimedia.org/wikiped" alt="" />
+          <div className="footer_songInfo">
+            <h4>Yeah</h4>
+            <p>...</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer__center">
+        <Shuffle className="footer__green" />
+        <SkipPrevious className="footer__icon" />
+        <PlayCircleFilledOutlined fontSize="large" className="footer__green" />
+        <SkipNext className="footer__icon" />
+        <Repeat className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlay />
+          </Grid>
+          <Grid item>
+            <VolumeDown />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledy="continuos-slide" />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
