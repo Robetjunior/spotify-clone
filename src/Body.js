795bc02cd12bba49fa180f38c061a30e2193c59b
Jose Roberto <jose.ferreira@77sol.com.br>
2021-11-11 03:29:28 -0300

    update SongRow

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Body.css';
 import Header from './Header';
 import { useStateValue } from './DataLayer';
+import { Favorite, MoreHoriz, PlayCircleFilled } from '@material-ui/icons';
+import SongRow from './SongRow';
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useStateValue();
@@ -17,6 +19,18 @@ function Body({ spotify }) {
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilled className="body__shuffle" />
+          <Favorite fontSize="large" />
+          <MoreHoriz />
+        </div>
+
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
